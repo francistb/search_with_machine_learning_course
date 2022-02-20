54e880fc6c1af82c38a26314cccc6cae99f3df7f
Francis Turgeon-Boutin <francis.turgeon.boutin@gmail.com>
2022-02-20 02:50:19 +0000

    Finish week1

--- gitpod-command.sh
+++ gitpod-command.sh
@@ -7,7 +7,6 @@ sudo chown -R gitpod:gitpod /workspace/opensearch
 sudo  chown -R gitpod:gitpod /workspace/logstash
 ln -s /home/gitpod/logstash-7.13.2 /workspace/logstash/logstash-7.13.2
 
-
 cd docker
 docker-compose up -d
 
--- week1/search.py
+++ week1/search.py
@@ -13,23 +13,13 @@ bp = Blueprint("search", __name__, url_prefix="/search")
 # display_filters -- return an array of filters that are applied that is appropriate for display
 # applied_filters -- return a String that is appropriate for inclusion in a URL as part of a query string.  This is basically the same as the input query string
 def process_filters(filters_input):
-    # Filters look like: &filter.name=regularPrice&regularPrice.key={{ agg.key }}&regularPrice.from={{ agg.from }}&regularPrice.to={{ agg.to }}
     filters = []
-    display_filters = (
-        []
-    )  # Also create the text we will use to display the filters that are applied
+    display_filters = []
     applied_filters = ""
     for filter in filters_input:
         type = request.args.get(filter + ".type")
         display_name = request.args.get(filter + ".displayName")
         filter_key = request.args.get(filter + ".key")
-
-        # We need to capture and return what filters are already applied so they can be automatically added to any existing links we display in aggregations.jinja2
-        # applied_filters += f"&filter.name={filter}&{filter}.type={type}&{filter}.displayName={display_name}"
-
-        # TODO: IMPLEMENT AND SET filters, display_filters and applied_filters.
-        # filters get used in create_query below.  display_filters gets used by display_filters.jinja2 and applied_filters gets used by aggregations.jinja2 (and any other links that would execute a search.)
-
         display_filters.append(f"{display_name} : {filter_key}")
 
         if type == "range":
@@ -43,11 +33,10 @@ def process_filters(filters_input):
                 range_filter[filter]["lt"] = to_filter
 
             filters.append({"range": range_filter})
-            applied_filters += f"&filter.name={filter}&{filter}.type={type}&{filter}.displayName={display_name}&{filter}.key={filter_key}&{filter}.from={from_filter}&filter.to={to_filter}"
+            applied_filters += f"&filter.name={filter}&{filter}.type={type}&{filter}.displayName={display_name}&{filter}.key={filter_key}&{filter}.from={from_filter}&{filter}.to={to_filter}"
         elif type == "terms":
             # filter.name=department&department.type=terms&department.key=PHOTO/COMMODITIES&department.displayName=Department
-            terms_filter = {f"{filter}.keyword": filter_key}
-            filters.append({"term": terms_filter})
+            filters.append({"term": {f"{filter}.keyword": filter_key}})
             applied_filters += f"&filter.name={filter}&{filter}.type={type}&{filter}.displayName={display_name}&{filter}.key={filter_key}"
 
     print("Filters: {}".format(filters))
@@ -118,22 +107,21 @@ def query():
 
 def create_query(user_query, filters, sort="_score", sortDir="desc"):
     print("Query: {} Filters: {} Sort: {}".format(user_query, filters, sort))
-    query_obj = {"size": 10, "track_total_hits": True}
+    query_obj = {"size": 10, "track_total_hits": True, "query": {}}
 
+    match_clause = None
     if user_query and user_query != "*":
-        query_obj["query"] = {
+        match_clause = {
             "bool": {
                 "must": [
                     {
                         "multi_match": {
                             "query": user_query,
                             "fields": [
-                                "name.keyword^1000",
-                                "name^10",
-                                "shortDescription^5",
-                                "description",
-                                "features",
-                                "longDescription",
+                                "name^100",
+                                "shortDescription^50",
+                                "longDescription^10",
+                                "department",
                             ],
                         }
                     }
@@ -141,8 +129,43 @@ def create_query(user_query, filters, sort="_score", sortDir="desc"):
                 "filter": filters,
             }
         }
+    elif filters:
+        match_clause = {
+            "bool": {
+                "filter": filters,
+            }
+        }
     else:
-        query_obj["query"] = {"match_all": {}}
+        match_clause = {"match_all": {}}
+
+    query_obj["query"]["function_score"] = {
+        "query": match_clause,
+        "boost_mode": "multiply",
+        "score_mode": "avg",
+        "functions": [
+            {
+                "field_value_factor": {
+                    "field": "salesRankShortTerm",
+                    "missing": 100000000,
+                    "modifier": "reciprocal",
+                }
+            },
+            {
+                "field_value_factor": {
+                    "field": "salesRankMediumTerm",
+                    "missing": 100000000,
+                    "modifier": "reciprocal",
+                }
+            },
+            {
+                "field_value_factor": {
+                    "field": "salesRankLongTerm",
+                    "missing": 100000000,
+                    "modifier": "reciprocal",
+                }
+            },
+        ],
+    }
 
     query_obj["aggs"] = {
         "regularPrice": {
@@ -160,6 +183,6 @@ def create_query(user_query, filters, sort="_score", sortDir="desc"):
         "missing_images": {"missing": {"field": "image"}},
     }
 
-    query_obj["sort"] = ({sort: sortDir},)
+    query_obj["sort"] = {sort: sortDir}
 
     return query_obj
